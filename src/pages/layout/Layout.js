87501001064fae568bd6d090fbeb92e0064200a5
Bridget Louise McCavanagh <blmccavanagh@gmail.com>
2021-07-28 23:27:30 +0800

    Layout updated.

--- src/pages/layout/Layout.js
+++ src/pages/layout/Layout.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import Navigation from './Navigation'
+import React from 'react';
+import Navigation from './Navigation';
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
 
 export default function Layout(props) {
     return (
         <div>
+            
+            <Header></Header>
+
             <Navigation></Navigation>
 
+                {props.children}
 
-            {props.children}
-
-
-            footer
+            <Footer></Footer>
 
         </div>
     )
