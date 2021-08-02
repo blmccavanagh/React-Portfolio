ccab0a955084863bfb2e8e34a023e0654da1d25b
Bridget Louise McCavanagh <blmccavanagh@gmail.com>
2021-08-02 22:55:47 +0800

    File paths updated.

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Project from '../containers/portfolio/Project';
+import Project from '../components/portfolio/Project';
 
 export default function Portfolio() {
   return (
-    <div>portfolio page
-
-
+    <div>
       <Project/>
     </div>
   );
